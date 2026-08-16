0de17749a6b3fff8d172afc050e564cb345deabd
govindhere06-code <govindhere.06@gmail.com>
2026-08-16 18:15:39 +0530

    T07 - Step 4 - Updated main.py file

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.routes import health
+from app.routes import ingest
 
 app = FastAPI(
     title="HybridRAG API",
@@ -17,3 +18,4 @@ app.add_middleware(
 )
 
 app.include_router(health.router)
+app.include_router(ingest.router)
