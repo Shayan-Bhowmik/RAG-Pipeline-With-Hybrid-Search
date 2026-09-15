63f9e76ed41577cc5cf9563658f2df1fdd0c7adf
Shayan Bhowmik <shayan.bhowmik.pune@gmail.com>
2026-09-16 00:26:10 +0530

    Make CORS origin configurable via ALLOWED_ORIGINS env var

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,5 @@
 import logging
+import os
 
 from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
@@ -49,7 +50,7 @@ class ErrorEnvelopeMiddleware(BaseHTTPMiddleware):
 app.add_middleware(ErrorEnvelopeMiddleware)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=os.getenv("ALLOWED_ORIGINS", "http://localhost:3000").split(","),
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
